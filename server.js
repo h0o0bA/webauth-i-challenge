fc008c89320a912ee68e76ade9881a9ff2066223
Ihab Ali <ihab@live.com>
2019-08-21 07:23:11 -0500

    added post /api/register/ endpoint

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
+const bcrypt = require("bcryptjs");
+
+const UsersDb = require("./users/users-model.js");
 
 const server = express();
 
@@ -8,6 +11,21 @@ server.use(express.json());
 
 server.get("/", (req, res) => {
   res.send("<h2>Web Auth I Module Challenge!</h2>");
+});
+
+server.post("/api/register", (req, res) => {
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  UsersDb.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 module.exports = server;
