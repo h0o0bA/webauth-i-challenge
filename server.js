3caa331ab7ebc9880655afe6f3cd20e956fdc6f5
Ihab Ali <ihab@live.com>
2019-08-21 07:26:56 -0500

    added get '/api/users/' endpoint

--- server.js
+++ server.js
@@ -45,4 +45,14 @@ server.post("/api/login", (req, res) => {
     });
 });
 
+// protect /api/users so only clients that provide valid credentials can see the list of users
+// read the username and password from the headers instead of the body (can't send a body on a GET request)
+server.get("/api/users", (req, res) => {
+  UsersDb.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
 module.exports = server;
