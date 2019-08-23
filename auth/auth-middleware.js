4619fa1aeca11b16413c0a4c1da522c93e17be83
Ihab Ali <ihab@live.com>
2019-08-23 06:02:02 -0500

    added req.session.user to memorize user in session when login sucessfully and request it in auth-middleware.js

--- auth/auth-middleware.js
+++ auth/auth-middleware.js
@@ -1,26 +1,34 @@
-const bcrypt = require("bcryptjs"); /// <<<<<< install it and require it
-
-const UsersDb = require("../users/users-model.js");
-
-module.exports = auth;
-
-function auth(req, res, next) {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    UsersDb.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "Invalid Credentials!" });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
+module.exports = (req, res, next) => {
+  if (req.session && req.session.user) {
+    next();
   } else {
-    res.status(400).json({ message: "Please provide a valid credentials!" });
+    res.status(401).json({ message: "You are not authorized!!" });
   }
-}
+};
+
+// const bcrypt = require("bcryptjs"); /// <<<<<< install it and require it
+
+// const UsersDb = require("../users/users-model.js");
+
+// module.exports = auth;
+
+// function auth(req, res, next) {
+//   const { username, password } = req.headers;
+
+//   if (username && password) {
+//     UsersDb.findBy({ username })
+//       .first()
+//       .then(user => {
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//           next();
+//         } else {
+//           res.status(401).json({ message: "Invalid Credentials!" });
+//         }
+//       })
+//       .catch(error => {
+//         res.status(500).json(error);
+//       });
+//   } else {
+//     res.status(400).json({ message: "Please provide a valid credentials!" });
+//   }
+// }
